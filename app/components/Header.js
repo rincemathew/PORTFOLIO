a4706cc9def40aa349afb05b2d379f25ab1bf0bf
Rince Mathew <rincemathewmanattu@gmail.com>
2024-09-11 23:39:38 +0530

    responsive header

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,54 +1,40 @@
-
-
 import React from 'react'
 
 function Header() {
   return (
-    <nav className="bg-white border-b border-gray-200">
-            <div className="container mx-auto flex justify-between items-center py-3 px-4">
-                <a className="text-2xl font-bold flex items-center" href="index.html">
-                    <i className='uil uil-user mr-2'></i> Marvel
-                </a>
-
-                <button className="lg:hidden flex flex-col space-y-1.5" type="button" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="block w-6 h-0.5 bg-black"></span>
-                    <span className="block w-6 h-0.5 bg-black"></span>
-                    <span className="block w-6 h-0.5 bg-black"></span>
-                </button>
-
-                <div className="hidden lg:flex flex-grow items-center justify-between" id="navbarNav">
-                    <ul className="flex flex-col lg:flex-row lg:space-x-8">
-                        <li className="nav-item">
-                            <a href="#about" className="nav-link py-2 lg:py-0 hover:text-gray-600 transition-colors">
-                                <span data-hover="About">About</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#project" className="nav-link py-2 lg:py-0 hover:text-gray-600 transition-colors">
-                                <span data-hover="Projects">Projects</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#resume" className="nav-link py-2 lg:py-0 hover:text-gray-600 transition-colors">
-                                <span data-hover="Resume">Resume</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#contact" className="nav-link py-2 lg:py-0 hover:text-gray-600 transition-colors">
-                                <span data-hover="Contact">Contact</span>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <ul className="flex items-center space-x-4 ml-auto">
-                        <div className="flex items-center">
-                            <i className="color-mode-icon mr-2"></i>
-                            <span>Color mode</span>
-                        </div>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="#" className="h-8" alt="Logo" />
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">logo</span>
+    </a>
+    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <span className="sr-only">Open main menu</span>
+        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+        </li>
+        <li>
+          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
   )
 }
 
