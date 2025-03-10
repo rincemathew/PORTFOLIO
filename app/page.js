c7ea087a441f467ca9f70be701312e7288db7f64
Rince Mathew <rincemathewmanattu@gmail.com>
2025-03-10 22:42:22 +0530

    grid layout cmpete

--- app/page.js
+++ app/page.js
@@ -4,18 +4,14 @@ export default function Home() {
   return (
     <main id="top" className="light_theme">
         <article className="grid-container">
+          <div className="grid-me"><div className="card">6</div></div>
+          <div className="grid-projects">3</div>
+          <div className="grid-education">4</div>
+          <div className="grid-blog">7</div>
+          <div className="grid-work-ex">2</div>
+          <div className="grid-resume">5</div>
+          <div className="grid-contact-me">8</div>
           <div className="grid-themes">1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-          <div>10</div>
-          <div>11</div>
-          <div>12</div>
           {/* <Main></Main> */}
         </article>
     </main>
