5f0c53c38a31c6fea33b6191abbffca8b7978d94
Rince Mathew <rincemathewmanattu@gmail.com>
2025-08-21 14:03:18 +0530

    font me fixed

--- app/components/Me.js
+++ app/components/Me.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { Open_Sans } from 'next/font/google'
+import { Playwrite_AU_QLD } from 'next/font/google'
 
-const openSans = Open_Sans({
-  subsets: ['Playwrite AU QLD", cursive;'],
+// import the correct font
+const playwrite = Playwrite_AU_QLD({
+  subsets: ['latin'], // use 'latin' subset
   display: 'swap',
 })
 
 export default function Me() {
   return (
-    <div className='bg-[var(--fourth-color)] w-full h-full flex flex-col items-center justify-center'>
-      <h1 className='text-[var(--primary-color)] text-3xl font-bold px-6 py-2'>
+    <div className={`bg-[var(--fourth-color)] w-full h-full flex flex-col items-center justify-center ${playwrite.className}`}>
+      <h1 className="text-[var(--primary-color)] text-3xl font-bold px-6 py-2">
         Rince Mathew
       </h1>
-      <h6 className='text-[var(--primary-color)] text-3xl font-bold px-6 py-2'>
+      <h6 className="text-[var(--primary-color)] text-3xl font-bold px-6 py-2">
         Web Developer | Tech Enthusiast
       </h6>
     </div>
